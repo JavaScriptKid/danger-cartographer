9d69d5aa9367724434ca1b6adec76a7233bbf68f
Drew Conley <drewconley@gmail.com>
2016-04-13 21:10:34 -0500

    add corners

--- src/landscape/landscape-placement.js
+++ src/landscape/landscape-placement.js
@@ -11,8 +11,23 @@ import {rectangle} from '../skins/skins'
 
 class LandscapePlacement extends React.Component {
 
+
+    renderCorners() {
+        const cornerStyle = {
+            width: this.props.cellSize / 2,
+            height: this.props.cellSize / 2
+        }
+        return (
+            <div className="placement-corners-container">
+                <div style={cornerStyle} className="placement-corner-top-left"></div>
+                <div style={cornerStyle} className="placement-corner-top-right"></div>
+                <div style={cornerStyle} className="placement-corner-bottom-left"></div>
+                <div style={cornerStyle} className="placement-corner-bottom-right"></div>
+            </div>
+        )
+    }
+
     render() {
-        console.log(this.props.model)
         const model = this.props.model;
 
         const style = {
@@ -23,9 +38,10 @@ class LandscapePlacement extends React.Component {
         const width = (model.width * this.props.cellSize);
         const height = (model.height * this.props.cellSize);
         return (
-           <div style={style}>
-               {rectangle(width, height, model.skin.fill1)}
-           </div>
+            <div style={style} className="landscape-placement">
+                {rectangle(width, height, model.skin.fill1)}
+                {this.renderCorners()}
+            </div>
         );
     }
 }
