b3abb9bcd476e2866bb64ed8c82a635201eab137
Drew Conley <drewconley@gmail.com>
2016-04-17 09:10:44 -0500

    change table math

--- src/skins/skins.js
+++ src/skins/skins.js
@@ -19,16 +19,17 @@ export function table(cellSize, width, height, fill1, fill2) {
     //        </svg>
     //    </svg>
     //)
-    const half = cellSize / 2;
-    const four = cellSize * 0.125;
-    const twelve = cellSize * 0.375;
-    const twentynine = cellSize * 0.906;
-    const twentyeight = cellSize * 0.875;
+    const sixteen = cellSize ;
+    const thirtytwo = cellSize * 2;
+    const four = cellSize * 0.25;
+    const twelve = cellSize * 0.75;
+    const twentynine = cellSize * 1.81;
+    const twentyeight = cellSize * 1.75;
     return (
         <svg width={width} height={height}>
-            <polygon fill={fill1} points={`0,0  0,${cellSize}  ${four},${cellSize}  ${four},${twentynine}  ${twelve},${twentynine}  ${twelve},${cellSize}  ${half},${cellSize}  ${half},0`}></polygon>
+            <polygon fill={fill1} points={`0,0  0,${thirtytwo}  ${four},${thirtytwo}  ${four},${twentynine}  ${twelve},${twentynine}  ${twelve},${thirtytwo}  ${sixteen},${thirtytwo}  ${sixteen},0`}></polygon>
             <svg y={twentyeight}>
-                <rect fill="#03547c" opacity="0.5" width={width/2} height={width/4}></rect>
+                <rect fill="#03547c" opacity="0.5" width={width} height={width/2}></rect>
             </svg>
         </svg>
     )
